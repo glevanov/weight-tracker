5178ce53a962cc03db880b579fba042a52cd0a89
Grigoriy Levanov <gregory@levanov.io>
2024-07-29 20:58:43 +0200

    add minimal chart implementation

---
+++ src/static-data.ts
@@ -0,0 +1,174 @@
+export const weights = [
+  {
+    weight: 92,
+    timestamp: "2024-01-05T00:00:00.000Z",
+  },
+  {
+    weight: 88.6,
+    timestamp: "2024-02-05T00:00:00.000Z",
+  },
+  {
+    weight: 87.7,
+    timestamp: "2024-02-07T00:00:00.000Z",
+  },
+  {
+    weight: 88,
+    timestamp: "2024-02-08T00:00:00.000Z",
+  },
+  {
+    weight: 87.8,
+    timestamp: "2024-02-11T00:00:00.000Z",
+  },
+  {
+    weight: 87.9,
+    timestamp: "2024-02-12T00:00:00.000Z",
+  },
+  {
+    weight: 87.2,
+    timestamp: "2024-02-14T00:00:00.000Z",
+  },
+  {
+    weight: 87.8,
+    timestamp: "2024-02-15T00:00:00.000Z",
+  },
+  {
+    weight: 87.7,
+    timestamp: "2024-02-20T00:00:00.000Z",
+  },
+  {
+    weight: 87.3,
+    timestamp: "2024-02-22T00:00:00.000Z",
+  },
+  {
+    weight: 86.9,
+    timestamp: "2024-02-28T00:00:00.000Z",
+  },
+  {
+    weight: 87.7,
+    timestamp: "2024-03-03T00:00:00.000Z",
+  },
+  {
+    weight: 86.5,
+    timestamp: "2024-03-06T00:00:00.000Z",
+  },
+  {
+    weight: 86,
+    timestamp: "2024-03-08T00:00:00.000Z",
+  },
+  {
+    weight: 85.7,
+    timestamp: "2024-03-13T00:00:00.000Z",
+  },
+  {
+    weight: 86,
+    timestamp: "2024-03-20T00:00:00.000Z",
+  },
+  {
+    weight: 86.6,
+    timestamp: "2024-03-22T00:00:00.000Z",
+  },
+  {
+    weight: 86.2,
+    timestamp: "2024-03-23T00:00:00.000Z",
+  },
+  {
+    weight: 85.5,
+    timestamp: "2024-03-29T00:00:00.000Z",
+  },
+  {
+    weight: 85.4,
+    timestamp: "2024-04-05T00:00:00.000Z",
+  },
+  {
+    weight: 85.4,
+    timestamp: "2024-04-09T00:00:00.000Z",
+  },
+  {
+    weight: 84.9,
+    timestamp: "2024-04-15T00:00:00.000Z",
+  },
+  {
+    weight: 85.2,
+    timestamp: "2024-04-19T00:00:00.000Z",
+  },
+  {
+    weight: 84.9,
+    timestamp: "2024-04-21T00:00:00.000Z",
+  },
+  {
+    weight: 84.7,
+    timestamp: "2024-04-30T00:00:00.000Z",
+  },
+  {
+    weight: 84.7,
+    timestamp: "2024-05-04T00:00:00.000Z",
+  },
+  {
+    weight: 84.5,
+    timestamp: "2024-05-09T00:00:00.000Z",
+  },
+  {
+    weight: 84.8,
+    timestamp: "2024-05-18T00:00:00.000Z",
+  },
+  {
+    weight: 84.3,
+    timestamp: "2024-05-20T00:00:00.000Z",
+  },
+  {
+    weight: 83.6,
+    timestamp: "2024-05-29T00:00:00.000Z",
+  },
+  {
+    weight: 82.5,
+    timestamp: "2024-06-02T00:00:00.000Z",
+  },
+  {
+    weight: 83.4,
+    timestamp: "2024-06-08T00:00:00.000Z",
+  },
+  {
+    weight: 83.3,
+    timestamp: "2024-06-17T00:00:00.000Z",
+  },
+  {
+    weight: 82.8,
+    timestamp: "2024-06-20T00:00:00.000Z",
+  },
+  {
+    weight: 82.4,
+    timestamp: "2024-06-27T00:00:00.000Z",
+  },
+  {
+    weight: 83.2,
+    timestamp: "2024-07-07T00:00:00.000Z",
+  },
+  {
+    weight: 82.4,
+    timestamp: "2024-07-11T00:00:00.000Z",
+  },
+  {
+    weight: 82.3,
+    timestamp: "2024-07-15T00:00:00.000Z",
+  },
+  {
+    weight: 81.9,
+    timestamp: "2024-07-17T00:00:00.000Z",
+  },
+  {
+    weight: 82.2,
+    timestamp: "2024-07-20T00:00:00.000Z",
+  },
+  {
+    weight: 82.2,
+    timestamp: "2024-07-21T00:00:00.000Z",
+  },
+  {
+    weight: 82.5,
+    timestamp: "2024-07-26T05:18:07.596Z",
+  },
+  {
+    weight: 82,
+    timestamp: "2024-07-28T06:12:55.503Z",
+  },
+];
