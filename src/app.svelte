db5bc07509772b1585ade915774e10fe528bcf6b
Grigoriy Levanov <gregory@levanov.io>
2024-07-29 22:32:07 +0200

    extract chart component

--- src/app.svelte
+++ src/app.svelte
@@ -1,45 +1,9 @@
 <script lang="ts">
-  import { Line } from "svelte-chartjs";
-  import { weights } from "./static-data";
-
-  import {
-    Chart as ChartJS,
-    LineElement,
-    LinearScale,
-    PointElement,
-    CategoryScale,
-  } from "chart.js";
-
-  ChartJS.register(LineElement, LinearScale, PointElement, CategoryScale);
-
-  const weightData: number[] = [];
-  const labels: string[] = [];
-
-  for (const entry of weights) {
-    weightData.push(entry.weight);
-    labels.push(
-      new Date(entry.timestamp).toLocaleDateString("ru-RU", {
-        month: "short",
-        day: "numeric",
-      }),
-    );
-  }
-
-  const chartData = {
-    labels: labels,
-    datasets: [
-      {
-        label: "График веса",
-        data: weightData,
-        borderColor: "#303F9F",
-        backgroundColor: "#303F9F",
-      },
-    ],
-  };
+  import Chart from "./chart.svelte";
 </script>
 
 <div class="app">
-  <Line data={chartData} />
+  <Chart />
 </div>
 
 <style>
