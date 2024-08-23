ca29954d8e07286b8209b4a6ac5a79af962e13ac
Grigoriy Levanov <gregory@levanov.io>
2024-08-23 17:55:08 +0200

    add dummy login page

--- src/app.svelte
+++ src/app.svelte
@@ -1,16 +1,19 @@
 <script lang="ts">
+  import Login from "./login/login.svelte";
   import Visualization from "./visualization/visualization.svelte";
   import AddWeight from "./add-weight/add-weight.svelte";
   import ToastContainer from "./ui/toast-container.svelte";
 
-  type Screen = "addWeight" | "chart";
-  let currentScreen: Screen = "addWeight";
+  type Screen = "addWeight" | "chart" | "login";
+  let currentScreen: Screen = "login";
   const showChart = () => (currentScreen = "chart");
   const showAddWeight = () => (currentScreen = "addWeight");
 </script>
 
 <div class="app">
-  {#if currentScreen === "addWeight"}
+  {#if currentScreen === "login"}
+    <Login />
+  {:else if currentScreen === "addWeight"}
     <AddWeight handleShowGraph={showChart} />
   {:else if currentScreen === "chart"}
     <Visualization handleShowAddWeight={showAddWeight} />
---
+++ src/login/login.svelte
@@ -0,0 +1,26 @@
+<script lang="ts">
+  const onSubmit = async () => {
+    console.log("submit");
+  };
+</script>
+
+<div class="page">
+  <form class="form" on:submit|preventDefault={onSubmit}>
+    <label for="login">Логин</label>
+    <input type="text" name="login" id="login" />
+
+    <label for="password">Пароль</label>
+    <input type="password" name="password" id="password" />
+  </form>
+</div>
+
+<style>
+  .page {
+    height: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding: 1em;
+    box-sizing: border-box;
+  }
+</style>
